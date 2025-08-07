a58a8aecccd9956d4ba30268e315e5f042584094
dahuan-wei <da_huan_wei@163.com>
2025-08-07 16:29:47 +0200

    nothing

---
+++ 20_classInLibraryAsInOpenFOAM/2_appWithClassInLibraryDynamicSeparate/appWithClassInLibrary.C
@@ -0,0 +1,17 @@
+#include <iostream> //Just for cout
+using namespace std; //Just for cout
+#include "myClass.H"
+int main()
+{
+myClass myClassObject;
+cout<< "myClassObject.i_: " << myClassObject.i_ << endl;
+cout<< "myClassObject.j_: " << myClassObject.j_ << endl;
+myClass myClassObject2;
+cout<< "myClassObject2.i_: " << myClassObject2.i_ << endl;
+myClassObject2.i_=30;
+cout<< "myClassObject.i_: " << myClassObject.i_ << endl;
+cout<< "myClassObject2.i_: " << myClassObject2.i_ << endl;
+cout<< " i am here  in seperate path now wow! " << endl;
+
+return 0;
+}
